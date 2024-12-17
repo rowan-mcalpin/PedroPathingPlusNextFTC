95b0b14cc23c1f0b4a2e6c3b993ad0ec2bd08124
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2024-12-16 20:16:09 -0600

    Create Drivetrain Subsystem as a subsystem for FollowPath and DriverControlled

--- TeamCode/src/main/java/com/rowanmcalpin/nextftc/ftc/DriverControlled.kt
+++ TeamCode/src/main/java/com/rowanmcalpin/nextftc/ftc/DriverControlled.kt
@@ -5,6 +5,7 @@ import com.rowanmcalpin.nextftc.core.command.Command
 import com.rowanmcalpin.nextftc.core.command.FollowerNotInitializedException
 import com.rowanmcalpin.nextftc.ftc.gamepad.GamepadEx
 import com.rowanmcalpin.nextftc.ftc.gamepad.Joystick
+import com.rowanmcalpin.nextftc.ftc.pedro.Drivetrain
 
 
 /**
@@ -13,64 +14,29 @@ import com.rowanmcalpin.nextftc.ftc.gamepad.Joystick
  * @param driveJoystick The joystick to use for forward and strafe movement
  * @param turnJoystick The joystick to use for turning
  * @param robotCentric Whether to use robot centric or field centric movement
- * @param subsystems The list of subsystems this command interacts with (should be whatever
- *  *                      subsystem holds this command)
  */
-class DriverControlled(val driveJoystick: Joystick, val turnJoystick: Joystick, val robotCentric: Boolean, override val subsystems: Set<Subsystem>): Command() {
+class DriverControlled(val driveJoystick: Joystick, val turnJoystick: Joystick, val robotCentric: Boolean): Command() {
 
     /**
      * @param driveJoystick The joystick to use for forward and strafe movement
      * @param turnJoystick The joystick to use for turning
-     * @param robotCentric Whether to use robot centric or field centric movement
-     * @param subsystem The subsystem this command interacts with
      */
-    constructor(driveJoystick: Joystick, turnJoystick: Joystick, robotCentric: Boolean, subsystem: Subsystem): this(driveJoystick, turnJoystick, robotCentric, setOf(subsystem))
-
-    /**
-     * @param driveJoystick The joystick to use for forward and strafe movement
-     * @param turnJoystick The joystick to use for turning
-     * @param subsystems The list of subsystems this command interacts with (should be whatever
-     *  *                      subsystem holds this command)
-     */
-    constructor(driveJoystick: Joystick, turnJoystick: Joystick, subsystems: Set<Subsystem>): this(driveJoystick, turnJoystick, true, subsystems)
-
-    /**
-     * @param driveJoystick The joystick to use for forward and strafe movement
-     * @param turnJoystick The joystick to use for turning
-     * @param subsystem The subsystem this command interacts with
-     */
-    constructor(driveJoystick: Joystick, turnJoystick: Joystick, subsystem: Subsystem): this(driveJoystick, turnJoystick, true, setOf(subsystem))
+    constructor(driveJoystick: Joystick, turnJoystick: Joystick): this(driveJoystick, turnJoystick, true)
 
     /**
      * @param gamepad The gamepad to use the joysticks from
      * @param robotCentric Whether to use robot centric or field centric movement
-     * @param subsystems The list of subsystems this command interacts with (should be whatever
-     *  *                     subsystem holds this command)
      */
-    constructor(gamepad: GamepadEx, robotCentric: Boolean, subsystems: Set<Subsystem>): this(gamepad.leftStick, gamepad.rightStick, robotCentric, subsystems)
+    constructor(gamepad: GamepadEx, robotCentric: Boolean): this(gamepad.leftStick, gamepad.rightStick, robotCentric)
 
     /**
      * @param gamepad The gamepad to use the joysticks from
-     * @param robotCentric Whether to use robot centric or field centric movement
-     * @param subsystem The subsystem this command interacts with
      */
-    constructor(gamepad: GamepadEx, robotCentric: Boolean, subsystem: Subsystem): this(gamepad.leftStick, gamepad.rightStick, robotCentric, setOf(subsystem))
-
-    /**
-     * @param gamepad The gamepad to use the joysticks from
-     * @param subsystems The list of subsystems this command interacts with (should be whatever
-     *  *                      subsystem holds this command)
-     */
-    constructor(gamepad: GamepadEx, subsystems: Set<Subsystem>): this(gamepad.leftStick, gamepad.rightStick, true, subsystems)
-
-    /**
-     * @param gamepad The gamepad to use the joysticks from
-     * @param subsystem The subsystem this command interacts with
-     */
-    constructor(gamepad: GamepadEx, subsystem: Subsystem): this(gamepad.leftStick, gamepad.rightStick, true, setOf(subsystem))
-
+    constructor(gamepad: GamepadEx): this(gamepad.leftStick, gamepad.rightStick, true)
 
     override val isDone: Boolean = false
+
+    override val subsystems: Set<Subsystem> = setOf(Drivetrain)
 
     override fun start() {
         if (OpModeData.follower == null) throw FollowerNotInitializedException()
