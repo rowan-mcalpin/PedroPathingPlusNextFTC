1d557332356198ca31d92f8c803cfa314b111781
Rowan McAlpin <87948250+rowan-mcalpin@users.noreply.github.com>
2024-12-16 23:23:59 -0800

    Hold position commands

---
+++ TeamCode/src/main/java/com/rowanmcalpin/nextftc/ftc/hardware/MotorHoldPosition.kt
@@ -0,0 +1,38 @@
+package com.rowanmcalpin.nextftc.ftc.hardware
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.rowanmcalpin.nextftc.core.Subsystem
+import com.rowanmcalpin.nextftc.core.command.Command
+import com.rowanmcalpin.nextftc.core.control.Controller
+import com.rowanmcalpin.nextftc.core.control.PIDFController
+import kotlin.math.abs
+
+/**
+ * This command holds a motor's position until another command is scheduled that uses the same
+ * subsystem.
+ * @param motor the motor to control
+ * @param controller the controller to implement
+ * @param subsystems the list of subsystems this command interacts with (should be whatever
+ *                      subsystem holds this command)
+ */
+class MotorHoldPosition(val motor: DcMotorEx, val controller: PIDFController,
+                        override val subsystems: Set<Subsystem>): Command() {
+    override val isDone = false
+    
+    constructor(motor: DcMotorEx, controller: PIDFController, subsystem: Subsystem): this(motor, controller, setOf(subsystem))
+
+    override fun start() {
+        controller.target = motor.currentPosition.toDouble()
+        controller.reset()
+    }
+    
+    override fun update() {
+        motor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        val calculatedPower = controller.calculate(motor.currentPosition.toDouble())
+        if (abs(motor.power - calculatedPower) > 0.001) {
+            motor.power = calculatedPower
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/com/rowanmcalpin/nextftc/ftc/hardware/MultipleMotorsHoldPosition.kt
@@ -0,0 +1,42 @@
+package com.rowanmcalpin.nextftc.ftc.hardware
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.rowanmcalpin.nextftc.core.Subsystem
+import com.rowanmcalpin.nextftc.core.command.Command
+import com.rowanmcalpin.nextftc.core.control.PIDFController
+import kotlin.math.abs
+
+/**
+ * This command holds a multiple motor's positions until another command is scheduled that uses the 
+ * same subsystem.
+ * 
+ * @param motorsAndControllers a map of all motors and their corresponding controllers
+ * @param subsystems the list of subsystems this command interacts with (should be whatever
+ *                      subsystem holds this command)
+ */
+class MultipleMotorsHoldPosition(val motorsAndControllers: Map<DcMotorEx, PIDFController>,
+                                 override val subsystems: Set<Subsystem>): Command() {
+    override val isDone = false
+
+    override fun start() {
+        if (motorsAndControllers.isEmpty()) {
+            throw IllegalArgumentException("You must specify at least one pair of motors and controllers")
+        }
+
+        motorsAndControllers.forEach {
+            it.value.target = it.key.currentPosition.toDouble()
+            it.value.reset()
+        }
+    }
+
+    override fun update() {
+        motorsAndControllers.forEach {
+            it.key.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+            val calculatedPower = it.value.calculate(it.key.currentPosition.toDouble())
+            if (abs(it.key.power - calculatedPower) > 0.001) {
+                it.key.power = calculatedPower
+            }
+        }
+    }
+}
